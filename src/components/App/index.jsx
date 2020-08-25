0ac9a6018b5475e4f06d94d450ba0c667a5fbbb0
Andrey L <forw39@gmail.com>
2020-08-25 13:32:51 +0200

    added fonts and changed some styles

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import Banner from '../Banner';
 import data from '../../data';
-import './styles.css'
-
+import './styles.css';
 
 const App = () => {
-  const banners = data.map(item => <Banner key={item.id} item={item} /> )
+  const banners = data.map((item) => <Banner key={item.id} item={item} />);
   return (
     <div className="wrapper">
       <div className="container">
         <h2 className="section-title">Ты сегодня покормил кота?</h2>
         <div className="main">
-          <ul className="banner__list">
-            {banners}
-          </ul>
+          <ul className="banner__list">{banners}</ul>
         </div>
       </div>
     </div>
   );
-}
+};
 export default App;
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,39 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import './styles.css';
 
 const Banner = (props) => {
-  const bannerStateText = (inStock, selectedItem, taste) => {
-    const buy = 'Чего сидишь? Порадуй котэ, купи';
-    const selected = 'Головы щучьи с чесноком да свежайшая семгушка';
-    const unavailable = `Печалька, ${taste} закончился`;
+  const { item } = props;
+  const { bannerText } = item;
+  const isSelectedBanner = !item.isSelected && item.isInStock;
+  const isNotSelectedBanner = item.isSelected && item.isInStock;
 
-    if(inStock === true && selectedItem === false) {return buy}
-    else if(inStock === true && selectedItem === true){return selected}
-    else if(inStock === false) {return unavailable}
-  };
   return (
     <li className="banner__item">
-      <div className="content">
-        <div className="information">
-          <span className="common-text">{props.item.subtitle}</span>
-          <h1 className="headline">{props.item.title}</h1>
-          <h2 className="subtitle">{props.item.ingredients}</h2>
-          <span className="common-text">{props.item.portion}</span>
-          <span className="common-text">{props.item.gift}</span>
-          <span className="common-text">{props.item.slogan}</span>
-        </div>
-        <div className="pictures">
-          <img src={props.item.image} alt="" className="pictures__img" />
-          <div className="mass">
-            <span className="weight">{props.item.quantity}</span>
-            <span className="weight weight-unit">{props.item.unit}</span>
+      <div className="card">
+        <div className="content">
+          <div className="information">
+            <span className="common-text subtitle">{item.subtitle}</span>
+            <h1 className="headline common-text--fat">{item.title}</h1>
+            <h2 className="taste common-text--fat">{item.ingredients}</h2>
+            <div className="description">
+              <span className="common-text common-text--fat common-text--amount">
+                {item.portionAmount}
+              </span>
+              <span className="common-text">{item.portionText}</span>
+            </div>
+            <div className="description">
+              <span className="common-text common-text--fat common-text--amount">
+                {item.giftAmount}
+              </span>
+              <span className="common-text">{item.giftText}</span>
+            </div>
+            <span className="common-text description">{item.slogan}</span>
+          </div>
+          <div className="pictures">
+            <img src={item.image} alt="" className="pictures__img" />
+            <div className="mass">
+              <span className="weight">{item.quantity}</span>
+              <span className="weight weight-unit">{item.unit}</span>
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <span className="common-text">{bannerStateText(true, true)}</span>
+      <div className="action">
+        <span className="action-text">
+          {isSelectedBanner && bannerText.notSelected}
+          {isNotSelectedBanner && bannerText.selected}
+          {!item.isInStock && bannerText.notInStock}
+        </span>
       </div>
     </li>
   );
 };
+
+Banner.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    ingredients: PropTypes.string.isRequired,
+    portionAmount: PropTypes.string.isRequired,
+    portionText: PropTypes.string.isRequired,
+    giftAmount: PropTypes.string.isRequired,
+    giftText: PropTypes.string.isRequired,
+    slogan: PropTypes.string.isRequired,
+    quantity: PropTypes.string.isRequired,
+    unit: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    isInStock: PropTypes.bool.isRequired,
+    isSelected: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
 export default Banner;
